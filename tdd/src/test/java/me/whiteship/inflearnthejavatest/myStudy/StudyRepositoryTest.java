294bb49a365658d3bc732e2922fe79cebaa4daec
JangHyunJin <winckey3@gmail.com>
2022-06-19 22:51:26 +0900

    tdd study

---
+++ tdd/src/test/java/me/whiteship/inflearnthejavatest/myStudy/StudyRepositoryTest.java
@@ -0,0 +1,28 @@
+package me.whiteship.inflearnthejavatest.myStudy;
+
+import me.whiteship.inflearnthejavatest.domain.Study;
+import me.whiteship.inflearnthejavatest.study.StudyRepository;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@DataJpaTest
+class StudyRepositoryTest {
+
+    @Autowired
+    StudyRepository repository;
+
+    @Test
+    void save() {
+        repository.deleteAll();
+        Study study = new Study(10, "Java");
+        repository.save(study);
+        List<Study> all = repository.findAll();
+        assertEquals(1, all.size());
+    }
+
+}
