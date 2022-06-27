c7ae3432c07c3bb0cf704763e42dc298388c3732
JangHyunJin <winckey3@gmail.com>
2022-06-27 23:50:59 +0900

    today study

---
+++ tdd/src/main/java/me/whiteship/inflearnthejavatest/domain/Account.java
@@ -0,0 +1,70 @@
+package me.whiteship.inflearnthejavatest.domain;
+
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+public class Account {
+
+    @Getter private final AccountId id;
+
+    @Getter private final Money baselineBalance;
+
+    @Getter private final ActivityWindow activityWindow;
+
+    public static Account account(
+            AccountId accountId,
+            Money baselineBalance,
+            ActivityWindow activityWindow) {
+        return new Account(accountId, baselineBalance, activityWindow);
+    }
+
+    public Optional<AccountId> getId(){
+        return Optional.ofNullable(this.id);
+    }
+
+    public Money calculateBalance() {
+        return Money.add(
+                this.baselineBalance,
+                this.activityWindow.calculateBalance(this.id));
+    }
+
+    public boolean withdraw(Money money, AccountId targetAccountId) {
+
+        if (!mayWithdraw(money)) {
+            return false;
+        }
+
+        Activity withdrawal = new Activity(
+                this.id,
+                this.id,
+                targetAccountId,
+                LocalDateTime.now(),
+                money);
+        this.activityWindow.addActivity(withdrawal);
+        return true;
+    }
+
+    private boolean mayWithdraw(Money money) {
+        return Money.add(
+                        this.calculateBalance(),
+                        money.negate())
+                .isPositiveOrZero();
+    }
+
+    public boolean deposit(Money money, AccountId sourceAccountId) {
+        Activity deposit = new Activity(
+                this.id,
+                sourceAccountId,
+                this.id,
+                LocalDateTime.now(),
+                money);
+        this.activityWindow.addActivity(deposit);
+        return true;
+    }
+
+    @Value
+    public static class AccountId {
+        private Long value;
+    }
+
+}
